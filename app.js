f676c7a252e0477cd9e8c58b0f0feb557d790db9
Colin Edwards <ColinJhEdwards@gmail.com>
2022-04-18 20:22:56 -0700

    practicing with simple add function for assinging types

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("Hello Typescript");
+console.log("Hello TypeScript");
+// working with types
+// CORE TYPES
+// Number - Only one number type in js&ts (1, 5.3, -10) all these are typeof number
+// String - can be defined with single quotes, double quotes, or back ticks. Simply text.
+// Boolean - true/false
+function add(n1, n2, showResult, phrase) {
+    if (showResult) {
+        console.log(phrase + n1 + n2);
+    }
+    else {
+        return n1 + n2;
+    }
+}
+var number1 = 5;
+var number2 = 3;
+var printResult = true;
+var phrase = "Result is: ";
+add(number1, number2, printResult, phrase);
